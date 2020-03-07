91eee241fb5aaa37b1aea477bf15f420ef1e5aae
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-07 14:56:09 +0000

    comments added

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,19 @@
 import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = () => {
   document.querySelector(".card").classList.add(generateRandomSuit());
   document.querySelector(".card").innerHTML = generateRandomNumber();
 };
-
+/* this generates a random number */
 let generateRandomNumber = () => {
   let numbers = ["A", "2", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let indexNumbers = Math.floor(Math.random() * numbers.length);
   return numbers[indexNumbers];
 };
-
+/* this generates a random suit */
 let generateRandomSuit = () => {
   let suit = ["diamond", "spade", "heart", "club"];
   let indexSuit = Math.floor(Math.random() * suit.length);
